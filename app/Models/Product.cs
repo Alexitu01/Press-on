a1e2f014d478e0d37ffd334e97f2e182f5e656f0
alhhITU <alhh@itu.dk>
2026-08-18 16:27:15 +0200

    Added the general structure of a Product, with a test setup for the main page

---
+++ app/Models/Product.cs
@@ -0,0 +1,18 @@
+using System.Security.Cryptography.X509Certificates;
+namespace Models;
+public class Product
+{
+    public int Id {get; set;}
+
+    public required string Name {get; set;}
+    
+    public double Price {get; set;}
+
+    public int Quantity {get; set;}
+
+    public int Size {get; set;}
+
+    public string? ImageUrl {get; set;}
+
+    public string? Details {get; set;}
+}
--- app/Pages/Index.cshtml.cs
+++ app/Pages/Index.cshtml.cs
@@ -1,12 +1,44 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
+using Models;
 
 namespace app.Pages;
 
 public class IndexModel : PageModel
 {
+
+    public List<Product> productList = new List<Product>();
+
     public void OnGet()
     {
+        productList.Add(new Product{
+            Name = "Sunrise",
+            Price = 20.20,
+            Quantity = 2,
+            Size = 10,
+            });
 
+        productList.Add(new Product{
+            Name = "Beach",
+            Price = 3,
+            Quantity = 2,
+            Size = 10,
+            });
+
+        productList.Add(new Product{
+            Name = "Night",
+            Price = 10,
+            Quantity = 2,
+            Size = 20,
+            });
+
+        productList.Add(new Product{
+            Name = "SOmething",
+            Price = 40,
+            Quantity = 2,
+            Size = 10,
+            ImageUrl = "imgs/nail1.png",
+            });
+        
     }
 }
